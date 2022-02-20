1d1e37424e6764ceebe5fa7a15fdeb70940c0e86
alexanderdavide <alexeble1998@gmail.com>
2022-02-20 22:01:41 +0000

    feat: add component pagination

---
+++ components/pagination.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="fixed p-2 text-xs"
+    v-bind:class="{
+      'left-0': $slidev.themeConfigs.paginationX === 'left',
+      'right-0': $slidev.themeConfigs.paginationX === 'right',
+      'top-0': $slidev.themeConfigs.paginationY === 'top',
+      'bottom-0': $slidev.themeConfigs.paginationY === 'bottom',
+    }"
+  >
+    <SlideCurrentNo /> / <SlidesTotal />
+  </div>
+</template>
