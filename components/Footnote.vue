cef139b35e71973acc003b2bc2bc213e3c9462ae
alexanderdavide <alexeble1998@gmail.com>
2022-02-24 21:16:07 +0000

    feat: add components Footnotes and Footnote

---
+++ components/Footnote.vue
@@ -0,0 +1,16 @@
+<template>
+  <li class="text-xs whitespace-nowrap">
+    <sup v-if="number">{{ number }}</sup>
+    <slot />
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    number: {
+      type: Number,
+    },
+  },
+};
+</script>
