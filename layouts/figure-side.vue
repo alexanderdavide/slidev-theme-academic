6a98eb13497f6dd2ad9b2aab471782f789abba6e
alexanderdavide <alexeble1998@gmail.com>
2022-02-26 22:20:22 +0000

    feat: add layout figure-side

---
+++ layouts/figure-side.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="grid grid-cols-2 h-full">
+    <div class="slidev-layout default">
+      <slot />
+    </div>
+    <FigureWithOptionalCaption
+      class="m-auto px-4"
+      :class="{
+        'order-first': figureX === 'l',
+      }"
+      :caption="figureCaption"
+      :footnoteNumber="figureFootnoteNumber"
+      :url="figureUrl"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { handleBackground } from "../layout-helper";
+
+const { figureUrl } = withDefaults(
+  defineProps<{
+    figureCaption?: string;
+    figureFootnoteNumber?: number;
+    figureUrl: string;
+    figureX?: "l" | "r";
+  }>(),
+  { figureX: "r" }
+);
+const style = computed(() => handleBackground(figureUrl));
+</script>
