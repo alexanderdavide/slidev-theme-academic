8b41a1e6e1f1bb8f0dbadb7272b4e3da1d3436d6
alexanderdavide <alexeble1998@gmail.com>
2022-02-26 22:14:54 +0000

    feat: add component FigureWithOptionalCaption

---
+++ components/FigureWithOptionalCaption.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="grid gap-3">
+    <img :alt="caption" :src="url" />
+    <p class="text-center text-xs" v-if="caption">
+      {{ caption }}<sup v-if="footnoteNumber">{{ footnoteNumber }}</sup>
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { handleBackground } from "../layout-helper";
+
+const props =
+  defineProps<{ caption?: string; footnoteNumber?: number; url: string }>();
+const style = computed(() => handleBackground(props.url));
+</script>
