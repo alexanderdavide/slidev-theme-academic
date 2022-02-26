d0ddd3f195919a4a44fc79c6dd6022fd78304945
alexanderdavide <alexeble1998@gmail.com>
2022-02-26 17:16:57 +0000

    feat: add component TextWithOptionalLink

---
+++ components/TextWithOptionalLink.vue
@@ -0,0 +1,8 @@
+<template>
+  <span v-if="!link">{{ text }}</span>
+  <a :href="link" rel="noreferrer" target="_blank" v-else>{{ text }}</a>
+</template>
+
+<script setup lang="ts">
+const { link, text } = defineProps<{ link?: string; text: string }>();
+</script>
