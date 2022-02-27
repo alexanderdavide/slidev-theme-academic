47822dadc9b37dbfd9c0d64c44472922a632d0b2
alexanderdavide <alexeble1998@gmail.com>
2022-02-27 19:42:42 +0000

    feat: add layout figure

---
+++ layouts/figure.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="slidev-layout default h-full">
+    <slot />
+    <FigureWithOptionalCaption
+      class="h-5/6 p-4"
+      :caption="figureCaption"
+      :footnoteNumber="figureFootnoteNumber"
+      :url="figureUrl"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  figureCaption?: string;
+  figureFootnoteNumber?: number;
+  figureUrl: string;
+}>();
+</script>
