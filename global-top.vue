8211c15a57b41588bcc3e3fe6ae3b6363a702e9b
alexanderdavide <alexeble1998@gmail.com>
2022-04-10 19:47:19 +0000

    feat: allow disabling rendering of global pagination for specific slides

--- global-top.vue
+++ global-top.vue
@@ -1,6 +1,9 @@
 <template>
   <Pagination
-    v-if="$slidev.themeConfigs.paginationX || $slidev.themeConfigs.paginationY"
+    v-if="
+      !$slidev.themeConfigs.paginationPagesDisabled.includes($slidev.nav.currentPage) &&
+      ($slidev.themeConfigs.paginationX || $slidev.themeConfigs.paginationY)
+    "
     :paginationX="$slidev.themeConfigs.paginationX"
     :paginationY="$slidev.themeConfigs.paginationY"
   />
