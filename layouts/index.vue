0ad399dd22320700753428e43cc7ab8b26efd90e
alexanderdavide <alexeble1998@gmail.com>
2022-04-12 20:19:01 +0000

    feat: add index

---
+++ layouts/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="slidev-layout">
+    <slot> <h1>Index</h1> </slot>
+    <TocList :level="1" :list="indexEntries" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const { indexEntries: indexEntriesTransferred } = defineProps<{
+  indexEntries: [{ pageNumber: number; title: string }];
+}>();
+
+const indexEntries = computed(() =>
+  indexEntriesTransferred.map(({ pageNumber, title }) => ({
+    children: [],
+    level: 1,
+    path: pageNumber.toString(),
+    title,
+  })),
+);
+</script>
