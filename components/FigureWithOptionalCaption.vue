b57a01dfc7363d99eeb41c556e97392b0c08b520
Delta-in-hub <DeltaMail@qq.com>
2022-12-02 11:12:15 +0800

    fix: consider base path specified by user

--- components/FigureWithOptionalCaption.vue
+++ components/FigureWithOptionalCaption.vue
@@ -1,6 +1,6 @@
 <template>
   <figure class="flex flex-col items-center justify-center">
-    <img :alt="caption" class="max-h-full" :src="url" />
+    <img :alt="caption" class="max-h-full" :src="resolveAssetUrl(url)" />
     <figcaption class="mt-3 text-center text-xs" v-if="caption">
       {{ caption }}<sup v-if="footnoteNumber">{{ footnoteNumber }}</sup>
     </figcaption>
@@ -8,5 +8,7 @@
 </template>
 
 <script setup lang="ts">
+import { resolveAssetUrl } from '../layout-helper';
+
 defineProps<{ caption?: string; footnoteNumber?: number; url: string }>();
 </script>
