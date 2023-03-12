f03443e78e38fcc316b1f2975e6d24bf0c563dcd
alexanderdavide <alexeble1998@gmail.com>
2023-03-12 18:13:10 +0000

    fix: ensure alignment of author, date and content
    resolves #10

--- layouts/cover.vue
+++ layouts/cover.vue
@@ -2,13 +2,13 @@
   <div class="slidev-layout cover" :style="style">
     <div class="my-auto w-full">
       <slot />
-    </div>
-    <div class="absolute bottom-12" v-if="coverAuthor || coverDate">
-      <TextWithOptionalLink :link="coverAuthorUrl" :text="coverAuthor" />
-      <span v-if="coverDate">{{ coverAuthor && coverDate ? `, ${coverDate}` : coverDate }}</span>
-    </div>
-    <div class="absolute bottom-0 font-extralight mb-1 mr-2 right-0 text-xs" v-if="coverBackgroundSource">
-      <TextWithOptionalLink :link="coverBackgroundSourceUrl" :text="coverBackgroundSource" />
+      <div class="absolute bottom-12" v-if="coverAuthor || coverDate">
+        <TextWithOptionalLink :link="coverAuthorUrl" :text="coverAuthor" />
+        <span v-if="coverDate">{{ coverAuthor && coverDate ? `, ${coverDate}` : coverDate }}</span>
+      </div>
+      <div class="absolute bottom-0 font-extralight mb-1 mr-2 right-0 text-xs" v-if="coverBackgroundSource">
+        <TextWithOptionalLink :link="coverBackgroundSourceUrl" :text="coverBackgroundSource" />
+      </div>
     </div>
   </div>
 </template>
